aae9a8d047fc77b17f8dcf77f87a82377874f362
harish-2006-ux <hhareeshvm@gmail.com>
2026-07-13 10:40:53 +0530

    first commit

---
+++ 3.R
@@ -0,0 +1,12 @@
+# --------------------------------------------
+# Question 4:
+# Create three variables using
+# = , <- and ->
+# Remove one variable using rm()
+# --------------------------------------------
+
+a = 10
+b <- 20
+30 -> c
+
+rm(b)
