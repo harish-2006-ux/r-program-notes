6dc7ed751f8ea163f6636a7448b00b31a1027a4e
Harish V M <hhareeshvm@gmail.com>
2026-07-24 09:17:42 +0530

    Add R script for vector operations

---
+++ session 1/13.R
@@ -0,0 +1,20 @@
+# --------------------------------------------
+# Question 13:
+# Perform:
+# Element-wise addition
+# Element-wise multiplication
+# Dot product
+# Compare vec1 > vec2
+# --------------------------------------------
+
+vec1 <- c(4,7,2,9,3)
+vec2 <- c(6,1,8,5,10)
+
+vec1 + vec2
+
+vec1 * vec2 
+
+sum(vec1 * vec2)
+
+vec1 > vec2
+
