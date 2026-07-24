f9d266529b2bc354a026b76d1de5ba33004ce30b
Harish V M <hhareeshvm@gmail.com>
2026-07-24 09:19:25 +0530

    Add character vector for months with operations
    Create a character vector for the months and perform basic operations.

---
+++ session 1/18.R
@@ -0,0 +1,16 @@
+# -------------------------------------------
+# Question 18:
+# Create character vector:
+# January
+# February
+# March
+#
+# Check type
+# Find length
+# Extract second element
+# -------------------------------------------
+
+month <- c("january","february","march")
+class(months)
+length(months)
+month[2]
